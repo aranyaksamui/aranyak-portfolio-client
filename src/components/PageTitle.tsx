e94d3c13b8e09c8769ff9e57f1c90f76ed5eea64
Aranyak Samui <aranyaksamuiarya@gmail.com>
2026-04-17 16:44:53 +0000

    Removed unused imports

--- src/components/PageTitle.tsx
+++ src/components/PageTitle.tsx
@@ -1,4 +1,4 @@
-import { IoArrowBackSharp, IoSearchSharp } from "react-icons/io5";
+import { IoArrowBackSharp } from "react-icons/io5";
 import { Link } from "react-router-dom";
 import { fallbackIcon, getSkillIcon } from "../utils/SkillIcons";
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "@fontsource/readex-pro";
 
 import "./index.css";
 import Root from "./App.tsx";
